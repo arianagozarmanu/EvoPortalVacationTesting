869a6e278af507864a0644b465309000a31f392f
danielavandor <danielavandor@EVZPC023.evointern>
2015-07-14 15:41:24 +0300

    DatDriven - Login Test

--- src/test/java/com/SignInValidTest.java
+++ src/test/java/com/SignInValidTest.java
@@ -6,15 +6,18 @@ import org.openqa.selenium.WebDriver;
 
 import com.steps.UserSteps;
 
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Issue;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.ManagedPages;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.annotations.UseTestDataFrom;
 import tools.Constants;
 
-@RunWith(SerenityRunner.class)
+@RunWith(SerenityParameterizedRunner.class)
+@UseTestDataFrom("/resources/accounts.csv")
 public class SignInValidTest {
 
     @Managed(uniqueSession = true)
@@ -25,13 +28,15 @@ public class SignInValidTest {
 
     @Steps
     public UserSteps endUser;
-
+    
+    String username, password;
+    
     @Issue("SignIn-ValidCredentials")
     
     @Test
     public void sign_in_should_be_valid() {
         endUser.is_the_home_page();
-		endUser.enters_data(Constants.VALID_SCREEN_NAME,Constants.VALID_PASSWORD);
+		endUser.enters_data(username,password);
 		endUser.signsIn();
 		endUser.sign_in_validation();
 		endUser.signsOut();
